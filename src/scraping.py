0be0df0e1d5d1912a6c73274e5af8467cdb956b8
lulidsnn <luizanepomuceno@alu.ufc.br>
2026-01-18 19:27:17 -0300

    feat: retirando arquivos do dataset antigo e alterando para o novo dataset

--- src/scraping.py
+++ src/scraping.py
@@ -1,37 +1,33 @@
 import kaggle
 import pandas as pd
 import json
+import os
+
+# identifica a pasta onde este script (scraping.py) está localizado (pasta src)
+src_folder = os.path.dirname(os.path.abspath(__file__))
+
+# define os caminhos completos para os arquivos
+csv_path = os.path.join(src_folder, "adult.csv")
+json_path = os.path.join(src_folder, "adult.json")
 
 # download do dataset do kaggle
 kaggle.api.dataset_download_files(
-    "mragpavank/breast-cancer",
-    path=".",
+    "wenruliu/adult-income-dataset",
+    path=src_folder,
     unzip=True
 )
 
-df = pd.read_csv("data.csv")
+df = pd.read_csv("adult.csv", na_values='?')
 
 # converte para lista de registros (scraping dos dados)
 records = df.to_dict(orient="records")
 
 # transforma o csv em um json
-with open("breast_cancer.json", "w", encoding="utf-8") as f:
+with open(json_path, "w", encoding="utf-8") as f:
     json.dump(records, f, indent=4, ensure_ascii=False)
 
 print("✔ Scraping concluído e JSON gerado!")
 
-# -----------------------------------------
-# remoção da coluna vazia no final do json
-# -----------------------------------------
-
 # carrega o JSON gerado pelo scraping
-with open("breast_cancer.json", "r", encoding="utf-8") as f:
+with open(json_path, "r", encoding="utf-8") as f:
     data = json.load(f)
-
-# remove a chave "Unnamed: 32" de cada registro
-for item in data:
-    item.pop("Unnamed: 32", None)  # None evita erro se não existir
-
-# salva novamente
-with open("breast_cancer.json", "w", encoding="utf-8") as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
